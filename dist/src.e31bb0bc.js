dad103052574bf80df19718f20539975463392b8
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2019-10-28 16:44:49 -0400

    updated content

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -31867,7 +31867,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55108" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51746" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
