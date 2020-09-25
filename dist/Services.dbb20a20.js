a412c595c2d6f5b2adcbc5b50d5e01cbdd862c32
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-09-25 13:26:01 -0400

    fix a typo

--- dist/Services.dbb20a20.js
+++ dist/Services.dbb20a20.js
@@ -20242,7 +20242,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58375" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58958" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
